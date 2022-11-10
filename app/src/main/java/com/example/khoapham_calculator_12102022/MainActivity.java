ea431774d8484c9dd6e535866c28904a7d8926b0
unknown <trongtoanbk.hd@gmail.com>
2022-11-11 06:21:41 +0700

    first commit

---
+++ app/src/main/java/com/example/khoapham_calculator_12102022/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.khoapham_calculator_12102022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
